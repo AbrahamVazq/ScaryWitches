0964946d15e0050f88a8e49e8cf8f21b74ace7ee
Abraham Vazquez <mabraham.vazquez@gmail.com>
2025-06-05 11:50:15 -0600

    Initial Commit

---
+++ ScaryWitches/ScaryWitchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaryWitchesApp.swift
+//  ScaryWitches
+//
+//  Created by Abraham Vazquez on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ScaryWitchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
