ed106ecf5dc4743195f74b1ba07e4a7764eedef2
Abraham Vazquez <mabraham.vazquez@gmail.com>
2025-06-09 17:21:10 -0600

    [UPDATE] Update the extension to SWProdutct

--- ScaryWitches/SWProduct.swift
+++ ScaryWitches/SWProduct.swift
@@ -16,3 +16,11 @@ struct SWProduct: Identifiable {
     let numReviews: Int
     let inCart: Bool
 }
+
+
+extension SWProduct {
+    static let sampleData: [SWProduct] = [
+        SWProduct(name: "Witch Hat", imgName: "witch-hat", descripcion: "A classic witch hat", price: 12.99, isFavorite: false, category: "Accessories", rating: 4.8, numReviews: 812, inCart: false),
+        SWProduct(name: "Chtulhu", imgName: "chtulhu-img", descripcion: "A Refresh of H.P. Lovecraft's classic drink", price: 25.99, isFavorite: true, category: "Drinks", rating: 4.8, numReviews: 12, inCart: false),
+        ]
+}
