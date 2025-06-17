d057c61c63569d28baf1de220115d8a538312386
Abraham Vazquez <mabraham.vazquez@gmail.com>
2025-06-17 13:15:54 -0600

    [ADD] Adding the  Enviroment to manage the Cart * Adding the SWCart Class to, add, delete and show the total to pay * Create the objecto, they living on all session

---
+++ ScaryWitches/SWCart.swift
@@ -0,0 +1,28 @@
+//  SWCart.swift
+//  ScaryWitches
+//  Created by Abraham Vazquez on 17/06/25.
+
+import SwiftUI
+
+class SWCart: ObservableObject {
+    
+    // 2. Usamos @Published. Este es el "ingrediente mágico".
+    // Cada vez que este array 'items' cambie (se añada o quite un producto),
+    // @Published notificará a todas las vistas que estén observando este objeto.
+    @Published var items: [SWProduct] = []
+    
+    // 3. Una propiedad computada para calcular el total fácilmente.
+    var totalPrice: Double {
+        items.reduce(0) { $0 + $1.price }
+    }
+    
+    // 4. Un método para añadir productos al carrito.
+    func add(_ product: SWProduct) {
+        items.append(product)
+    }
+    
+    // 5. Un método para eliminar productos (será útil para la vista del carrito).
+    func remove(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+    }
+}
--- ScaryWitches/ScaryWitchesApp.swift
+++ ScaryWitches/ScaryWitchesApp.swift
@@ -1,17 +1,21 @@
-//
 //  ScaryWitchesApp.swift
 //  ScaryWitches
-//
 //  Created by Abraham Vazquez on 05/06/25.
-//
 
 import SwiftUI
 
 @main
 struct ScaryWitchesApp: App {
+    
+    // 1. Creamos una instancia de nuestro carrito que vivirá durante toda la sesión de la app.
+    // Se usa @StateObject porque esta vista es la "dueña" del objeto.
+    @StateObject private var cart = SWCart()
+    
     var body: some Scene {
+        
         WindowGroup {
             ContentView()
         }
+        
     }
 }
